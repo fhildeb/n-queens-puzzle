975be70d4bc0bc69c826002a81a83b9254db0a01
fhildeb <felix@lukso.io>
2022-05-29 12:03:03 +0200

    add camera script

---
+++ Assets/Scripts/KameraDrehung.cs
@@ -0,0 +1,29 @@
+﻿using UnityEngine;
+using System.Collections;
+
+//@Felix Hildebrandt
+//Klasse welche die Kamerarotation ansteuert
+public class KameraDrehung : MonoBehaviour
+{
+
+    //@Felix Hildebrandt
+    //Kontinuierliche Aktualisierung der Kamerarotation
+    void Update()
+    {
+        float speed = 20f;
+
+        //Linke Taste gedrückt
+        if (Input.GetKey(KeyCode.LeftArrow))
+        {
+            transform.Rotate(0f, speed * Time.deltaTime, 0f);
+
+        }
+
+        //Linke Taste gedrückt
+        if (Input.GetKey(KeyCode.RightArrow))
+        {
+            transform.Rotate(0f, -speed * Time.deltaTime, 0f);
+
+        }
+    }
+}
