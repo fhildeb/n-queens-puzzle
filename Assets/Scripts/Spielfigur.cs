0de3cc286e6f64c2fc2e191c567dd233705086a3
fhildeb <felix@lukso.io>
2022-05-29 12:03:25 +0200

    add script for the queens game figure

---
+++ Assets/Scripts/Spielfigur.cs
@@ -0,0 +1,28 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+//@Felix Hildebrandt
+//Abstrakte Klasse von welcher alle Spielfiguren erben.
+//Definiert grundlegend benötigte Funktionen
+public abstract class Spielfigur : MonoBehaviour {
+
+    //Positionen auslesen
+    public int aktuelleXPos { set; get; }
+    public int aktuelleYPos { set; get; }
+
+    //@Felix Hildebrandt
+    //Positionen setzen
+    public void setPosition(int x, int y)
+    {
+        aktuelleXPos = x;
+        aktuelleYPos = y;
+    }
+
+    //@Felix Hildebrandt
+    //Prüfen in welche Richtung die Figur bewegt werden darf
+    public virtual bool[,] SpielzugGestattet()
+    {
+        return new bool[SpielflaechenManager.groesse,SpielflaechenManager.groesse];
+    }
+}
